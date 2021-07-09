0de394876fe4368534ab5bd2e0e10f0bde051f07
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-09 11:32:46 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,1 +1,15 @@
-buy premium api from @Sylixx
+<?php
+API BY @SYLIXX
+
+
+$getid = $_GET['ip'];
+
+   $url = "https://sylixapi.herokuapp.com/api/iplookup?q=$getid";
+   $curl = curl_init($url);
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json'
+   ]);
+   $response = curl_exec($curl);
+   curl_close($curl);
+   echo $response . PHP_EOL;
