e274ebe6a73a98676b701f6e9775d450b88e1f82
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-09 12:42:15 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,12 +1,24 @@
 <?php
-$getid = $_GET['ip'];
 
-   $url = "https://sylixapi.herokuapp.com/api/iplookup?q=$getid";
-   $curl = curl_init($url);
-   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($curl, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json'
-   ]);
-   $response = curl_exec($curl);
-   curl_close($curl);
-   echo $response . PHP_EOL;
+#api by @Sylixx 
+#@Sylix_team
+$ch = curl_init();
+$getid = $_GET['image_url'];
+$data = array('image' => "$getid");
+curl_setopt($ch, CURLOPT_URL, 'https://api.deepai.org/api/nsfw-detector');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+
+$headers = array();
+$headers[] = 'Api-Key: 2eafb2bf-243c-4098-9d72-cac2a90d8d2c';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Image url is wrong api by @Sylixx @SylixTeam';
+}
+else {
+    echo $result;
+}
+curl_close($ch);
