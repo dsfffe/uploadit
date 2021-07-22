9d09e93f4ff76f99708f7f935672b41b6090215b
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-22 11:17:31 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,24 +1,119 @@
 <?php
 
-#api by @Sylixx 
-#@Sylix_team
-$ch = curl_init();
-$getid = $_GET['image_url'];
-$data = array('image' => "$getid");
-curl_setopt($ch, CURLOPT_URL, 'https://api.deepai.org/api/nsfw-detector');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+/**
+ * @var Creator : T.me//MrSylix
+ * @version SylixTeam PHP Member Baner By MrSylix v1.0.0
+ */
 
-$headers = array();
-$headers[] = 'Api-Key: 2eafb2bf-243c-4098-9d72-cac2a90d8d2c';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Image url is wrong api by @Sylixx @SylixTeam';
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-else {
-    echo $result;
+
+include 'madeline.php';
+
+use \danog\MadelineProto\API;
+use \danog\Loop\Generic\GenericLoop;
+use \danog\MadelineProto\EventHandler;
+
+
+class XHandler extends EventHandler
+{
+    const Admins = [0 => 1476130628];
+    const Report = '@MrMahdiii';
+
+    public function getReportPeers()
+    {
+        return [self::Report];
+    }
+    public function genLoop()
+    {
+        yield $this->account->updateStatus([
+            'offline' => false
+        ]);
+        return 60000;
+    }
+
+    public function onStart()
+    {
+        $genLoop = new GenericLoop([$this, 'genLoop'], 'update Status');
+        $genLoop->start();
+    }
+
+    public function onUpdateNewChannelMessage($update)
+    {
+        yield $this->onUpdateNewMessage($update);
+    }
+    public function getMessages($update, $id = [])
+    {
+        try {
+            $info = yield $this->getInfo($update);
+
+            $method = in_array($info['type'], ['bot', 'user', 'chat']) ? 'messages' : 'channels';
+            $params = ['id' => $id];
+
+            if (in_array($info['type'], ['channel', 'supergroup'])) {
+                $params['channel'] = $update;
+            }
+
+            $get = yield $this->$method->getMessages($params);
+            return $get;
+        } catch (Throwable $e) {
+            //throw new Exception($e->getMessage());
+        }
+    }
+
+    public function onUpdateNewMessage($update)
+    {
+        if (time() - $update['message']['date'] > 2) {
+            return;
+        }
+        try {
+            $msg             = isset($update['message']) ? $update['message'] : null;
+            $txt             = isset($update['message']['message']) ? $update['message']['message'] : null;
+            $msg_id          = isset($update['message']['id']) ? $update['message']['id'] : null;
+            $reply_to_msg_id = isset($update['message']['reply_to']['reply_to_msg_id']) ? $update['message']['reply_to']['reply_to_msg_id'] : null;
+            $user_id         = isset($update['message']['from_id']['user_id']) ? $update['message']['from_id']['user_id'] : null;
+            $com             = isset($update['message']['fwd_from']['saved_from_peer']) ? true : false;
+            $me              = yield $this->getSelf();
+            $peer            = yield $this->getInfo($update);
+            $chID            = yield $this->getID($update);
+            $chat_id         = $peer['bot_api_id'];
+            $type            = $peer['type'];
+
+            
+            #ADMIN Commands
+            if (in_array($user_id, self::Admins) || $user_id == $me_id) {
+
+            
+                    
+
+       
+
+
+            }
+        } catch (\Throwable $e) {
+            //$this->report("Surfaced: $e");
+        }
+    }
 }
-curl_close($ch);
+
+$settings = [
+    'serialization' => [
+        'cleanup_before_serialization' => true,
+    ],
+    'logger' => [
+        'max_size' => 1 * 1024 * 1024,
+    ],
+    'peer' => [
+        'full_fetch' => false,
+        'cache_all_peers_on_startup' => false,
+    ],
+    'app_info' => [
+        'api_id' => 1374236,
+        'api_hash' => '4df50166bf37f939cd10c71ece878b06'
+    ]
+];
+
+$bot = new \danog\MadelineProto\API('X.session', $settings);
+$bot->startAndLoop(XHandler::class);
