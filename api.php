9bccf50ccae15ea49c6142b1b7f7340d92f7accb
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-22 20:39:59 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,32 +1,16 @@
 <?php
 
-// channel : @OGHAB_TM
+$url = "https://api.ocr.space/Parse/Image";
 
-date_default_timezone_set('Asia/Tehran');
-if (!file_exists('madeline.php')) {
-copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
-}
-define('MADELINE_BRANCH', '5.1.34');
-include 'madeline.php';
-$settings = [];
-$settings['app_info']['api_id'] = '394879';
-$settings['app_info']['api_hash'] = 'b45f10cd94d6f970f55fd17a3b400b8f';
-$MadelineProto = new \danog\MadelineProto\API('oghab.madeline', $settings);
-$MadelineProto->start();
-$time = date('H:i');
-try {
-  // بیو
- 
-  // انلاین
-  $MadelineProto->account->updateStatus(['offline' => false]);
-  $MadelineProto->account->updateProfile(['last_name' => " 〔 $time 〕"]);
-  // نام
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-} catch (\Exception $e) {
-  echo "$e";
-}
-sleep(2);
-if (file_exists('MadelineProto.log')) {
- unlink('MadelineProto.log');
-}
-echo 'Ok!';
+$headers = array(
+   "apikey: eb18cc1eca88957",
+   "Content-Type: application/x-www-form-urlencoded",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($curl);
+echo $result;
