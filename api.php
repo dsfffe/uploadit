89d5579b871f5ef21fffd577d1007900e0bfb3b2
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-22 12:09:29 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,125 +1,31 @@
 <?php
 
-/**
- * @var Creator : T.me//MrSylix
- * @version SylixTeam PHP Member Baner By MrSylix v1.0.0
- */
+// channel : @OGHAB_TM
 
-
+date_default_timezone_set('Asia/Tehran');
 if (!file_exists('madeline.php')) {
-    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-
+define('MADELINE_BRANCH', '5.1.34');
 include 'madeline.php';
+$settings = [];
+$settings['app_info']['api_id'] = '1059453';
+$settings['app_info']['api_hash'] = '910474397b399ffcc1d19364008ed274';
+$MadelineProto = new \danog\MadelineProto\API('oghab.madeline', $settings);
+$MadelineProto->start();
+$time = date('H:i');
+try {
+  // بیو
+ 
+  // انلاین
+  $MadelineProto->account->updateStatus(['offline' => false]);
+  // نام
 
-use \danog\MadelineProto\API;
-use \danog\Loop\Generic\GenericLoop;
-use \danog\MadelineProto\EventHandler;
-
-
-class XHandler extends EventHandler
-{
-    const Admins = [0 => 1476130628];
-    const Report = '@MrMahdiii';
-
-    public function getReportPeers()
-    {
-        return [self::Report];
-    }
-    public function genLoop()
-    {
-        yield $this->account->updateStatus([
-            'offline' => false
-        ]);
-        return 60000;
-    }
-
-    public function onStart()
-    {
-        $genLoop = new GenericLoop([$this, 'genLoop'], 'update Status');
-        $genLoop->start();
-    }
-
-    public function onUpdateNewChannelMessage($update)
-    {
-        yield $this->onUpdateNewMessage($update);
-    }
-    public function getMessages($update, $id = [])
-    {
-        try {
-            $info = yield $this->getInfo($update);
-
-            $method = in_array($info['type'], ['bot', 'user', 'chat']) ? 'messages' : 'channels';
-            $params = ['id' => $id];
-
-            if (in_array($info['type'], ['channel', 'supergroup'])) {
-                $params['channel'] = $update;
-            }
-
-            $get = yield $this->$method->getMessages($params);
-            return $get;
-        } catch (Throwable $e) {
-            //throw new Exception($e->getMessage());
-        }
-    }
-
-    public function onUpdateNewMessage($update)
-    {
-        if (time() - $update['message']['date'] > 2) {
-            return;
-        }
-        try {
-            $msg             = isset($update['message']) ? $update['message'] : null;
-            $txt             = isset($update['message']['message']) ? $update['message']['message'] : null;
-            $msg_id          = isset($update['message']['id']) ? $update['message']['id'] : null;
-            $reply_to_msg_id = isset($update['message']['reply_to']['reply_to_msg_id']) ? $update['message']['reply_to']['reply_to_msg_id'] : null;
-            $user_id         = isset($update['message']['from_id']['user_id']) ? $update['message']['from_id']['user_id'] : null;
-            $com             = isset($update['message']['fwd_from']['saved_from_peer']) ? true : false;
-            $me              = yield $this->getSelf();
-            $peer            = yield $this->getInfo($update);
-            $chID            = yield $this->getID($update);
-            $chat_id         = $peer['bot_api_id'];
-            $type            = $peer['type'];
-
-
-            if (isset($update['message']['fwd_from']['saved_from_peer'])){
-                yield $this->messages->sendMessage(['peer' => $chID, 'message' => 'bemola', 'parse_mode' => 'Markdown', 'reply_to_msg_id' => $msg_id]);
-                }
-                
-            
-            #ADMIN Commands
-            if (in_array($user_id, self::Admins) || $user_id == $me_id) {
-
-            
-                    
-
-       
-
-
-            }
-        } catch (\Throwable $e) {
-            //$this->report("Surfaced: $e");
-        }
-    }
+} catch (\Exception $e) {
+  echo "$e";
 }
-
-$settings = [
-    'serialization' => [
-        'cleanup_before_serialization' => true,
-    ],
-    'logger' => [
-        'max_size' => 1 * 1024 * 1024,
-    ],
-    'peer' => [
-        'full_fetch' => false,
-        'cache_all_peers_on_startup' => false,
-    ],
-    'app_info' => [
-        'api_id' => 1374236,
-        'api_hash' => '4df50166bf37f939cd10c71ece878b06'
-    ]
-];
-
-$bot = new \danog\MadelineProto\API('X.session', $settings);
-$bot->startAndLoop(XHandler::class);
-
+sleep(2);
+if (file_exists('MadelineProto.log')) {
+ unlink('MadelineProto.log');
+}
+echo 'Ok!';
