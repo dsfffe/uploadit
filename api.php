b36d422e6e7a6763f5326cfa8eddbf1ec3d93d5b
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-22 21:03:00 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,16 +1,8 @@
 <?php
+error_reporting(false);
+header('Content-type: application/json;');
+$urll = $_GET['url'];
+$array = json_decode(file_get_contents("https://api.ocr.space/parse/imageurl?apikey=eb18cc1eca88957&url=$urll&language=eng&detectOrientation=True&filetype=JPG&OCREngine=1&isTable=True&scale=True"), true);
+$result = $array['ParsedResults'][0]['ParsedText'];
 
-$url = "https://i.ibb.co/3vh3r5P/Screenshot-20210722-001839-Phone.jpg";
-
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$headers = array(
-   "apikey: eb18cc1eca88957",
-   "Content-Type: application/x-www-form-urlencoded",
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-$result = curl_exec($curl);
 echo $result;
